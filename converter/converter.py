ea3813703349a3420a4c793bd5c65522c2860603
AntoineZappa <antgonbla@gmail.com>
2020-10-05 18:09:55 +0200

    Converter modified. Fixed a problem with types.

--- converter/converter.py
+++ converter/converter.py
@@ -1,31 +1,34 @@
 #/usr/bin/env python
-import pytest
+
 
 def main():
-    mode = str(input('''\nType a number to select a conversion mode:
-
-    1: Decimal to Binary (with base 2) or any other base
-    2: Binary to decimal
-    3: Binary to complement 1
-    4: Binary to complement 2
+    mode = str(input('''\n
++------------------------------------------------------------+
+|    Type a number to select a conversion mode:              |
++------------------------------------------------------------+
+|    1: Decimal to Binary (with base 2) or any other base    |
+|    2: Binary to decimal                                    |   
+|    3: Binary to complement 1                               |
+|    4: Binary to complement 2                               |        
++------------------------------------------------------------+                          
 
 Mode: '''))
 
     if mode == '1':
         try:
-            numero = int(input('Input a number to change the base: '))
+            number = int(input('Input a number to change the base: '))
             base = int(input('Input a base: '))
             
         except ValueError:
             print("")
-        print(dec2bin(numero, base))
+        print(dec2bin(number, base))
 
     elif mode == '2':
-        binary = list(input('Input a number in binary: '))
+        binary = str(input('Input a number in binary: '))
         print(bin2dec(binary))
 
     elif mode == '3':
-        binary = list(input('Input a number in binary: '))
+        binary = str(input('Input a number in binary: '))
         print(bin2complement1(binary))
 
     elif mode == '4':
@@ -37,31 +40,54 @@ Mode: '''))
         main()
 
 def dec2bin(decimal, base):
+    """Convert from decimal to a number with a new base
 
+    Args:
+        decimal (int): number to change base
+        base (int): base
+
+    Returns:
+        str: str with the converted number
+    """
     conversion = ''
+
     while decimal // base != 0:
+
         conversion = str(decimal % base) + conversion
-        print(conversion)
         decimal = decimal // base
         result = str(decimal) + conversion
-    print(type(result))
+    
     return result
 
 
-
 def bin2dec(binary):
-    
+    """ Convert binary to decimal
+
+    Args:
+        binary (str): list of elements from binay  str
+
+    Returns:
+        str: str with the final decimal 
+    """
     value = 0
+    binary = list(binary)
     for i in range(len(binary)):
         n = binary.pop()
         if n == '1':
-            value = value + 2**i
-    print(type(value))
-    return value
+            value = value + pow(2,i)
+
+    return str(value)
     
 
 def bin2complement1(binary):
+    """Convert a binary in bincary two's compliment
 
+    Args:
+        binary (str): [description]
+
+    Returns:
+        str: str with the new binary
+    """
     result = []
     for i,n in enumerate(binary):
         if n == '1':
@@ -69,23 +95,29 @@ def bin2complement1(binary):
         elif n == '0':
             n = '1'
         result.append(n)
-    
-    return "".join(result)
+    integer = int("".join(result),2)
+    binfinal = bin(integer)
 
+    return str(binfinal).replace('0b', '')
 
 def bin2complement2(binary):
 
     result = []
     for i, n in enumerate(binary):
-        if i in range(len(binary)-2):
+        if i in range(len(binary)):
             if n == '1':
                 n = '0'
             elif n == '0':
                 n = '1'
         result.append(n)
+
+    numstr = "".join(result)
+    bin1 = int(numstr, 2)
+    bin2 = int('00000001',2)
     
-    return "".join(result)
+    binfinal = bin (bin1 + bin2)
+    
+    return str(binfinal).replace('0b','')
 
 if __name__ == "__main__":
     main()
-            
